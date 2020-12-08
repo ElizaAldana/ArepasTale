9c36804fda7eb59ec08eeb0d18debe65458167bd
Gabriel <gabrielmora2001@hotmail.com>
2020-12-08 17:35:14 -0500

    Paths de las clases a las imagenes añadidos
    se cargaron las imagenes de las screens en sus respectivas clases

---
+++ src/view/ScreenHome.java
@@ -0,0 +1,12 @@
+package view;
+
+import processing.core.PApplet;
+
+public class ScreenHome extends Screen{
+
+	public ScreenHome(PApplet app) {
+		super(app);
+		image= app.loadImage("pngs/Pantallas/Inicio.png");
+	}
+
+}
--- src/view/ScreenWin.java
+++ src/view/ScreenWin.java
@@ -6,6 +6,7 @@ public class ScreenWin extends Screen{
 
 	public ScreenWin(PApplet app) {
 		super(app);
+		image= app.loadImage("pngs/Pantallas/Win.png");
 		// TODO Auto-generated constructor stub
 	}
 
